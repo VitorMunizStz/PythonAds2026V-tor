5840dec8832523456aecc80d736f944f9bc4924f
Vitor Muniz <vitorlgmuniz@gmail.com>
2026-09-12 00:42:02 +0000

    Add initial scripts for greeting and user information display

---
+++ 2.py
@@ -0,0 +1,29 @@
+nome = "Vitinho"
+idade = 19
+altura = 1.60
+aprovado = True
+
+print("Seja bem-vindo ao Vitinho parque!")
+
+if nome == "Vitinho":
+    print("Olá, Vitinho!")
+else:
+    print("Olá, visitante!")
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+if altura >= 1.80:
+    print("Você é alto")
+else:
+    print("Você não é alto")
+
+#str significa string
+print(
+    f"{nome} tem {idade} anos, "
+    f"{'é maior de idade' if idade >= 18 else 'é menor de idade'}, "
+    f"mede {altura}m ({'é alto' if altura >= 1.70 else 'não é alto'}), e "
+    f"{'está aprovado' if aprovado else 'não está aprovado'}"
+)
---
+++ script.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("This is a test")
+print(5.2 + 3)
+print(5 + 2 == 7)
+print(5 + 2 == 8)
+
+nome1 = "Vitor"
+nome2 = "Vítor Lucas"
+nome3 = "Vitor Lucas Geloni"
+nome4 = "Vitor Lucas Geloni Muniz"
+
+print(nome1, nome2, nome3, nome4)
